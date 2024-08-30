04ea6cbc4a6619da9b3a4ab47ef3a79b5c8fb4a6
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-08-31 05:41:08 +0600

    Some real life example of inheritance

---
+++ inheritance.py
@@ -0,0 +1,29 @@
+# inheritance provides you "is a" relation
+
+class Animal:
+    pass
+
+# dog is an animal
+class Dog(Animal):
+    pass
+
+# tiger is an animal
+class Tiger(Animal):
+    pass 
+
+
+
+class Furniture:
+    pass 
+
+# Chair is a furniture
+class Chair(Furniture):
+    pass 
+
+# table is a furniture
+class Table(Furniture):
+    pass
+
+# Bed is a furnitur
+class Bed(Furniture):
+    pass 
